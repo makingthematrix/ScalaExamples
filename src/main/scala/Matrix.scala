7202e17f6d4a3e3177eabd7ce8454202d61e7568
makingthematrix <makingthematrix@protonmail.com>
2025-08-27 16:11:50 +0200

    adding Matrix for the invalid red code example

---
+++ src/main/scala/Matrix.scala
@@ -0,0 +1,11 @@
+opaque type Matrix = (array: Array[Int], cols: Int)
+object Matrix:
+  def apply(rows: Int, cols: Int): Matrix = (Array.fill(rows * cols)(0), cols)
+end Matrix
+
+extension (matrix: Matrix)
+  def get(row: Int, col: Int): Int = matrix.array(row * matrix.cols + col)
+
+def main(): Unit =
+  val matrix = Matrix(3, 3)
+  //matrix.g
