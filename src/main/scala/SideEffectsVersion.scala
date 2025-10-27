4d458391fbf9eb8b78bd045427804f725a223104
makingthematrix <makingthematrix@protonmail.com>
2025-10-27 18:08:00 +0100

    ArtistFan

--- src/main/scala/SideEffectsVersion.scala
+++ src/main/scala/SideEffectsVersion.scala
@@ -4,7 +4,7 @@ import scala.jdk.CollectionConverters.*
 object SideEffectsVersion {
   @main def main(): Unit = {
     val lines = read(Protagonist.FilePath)
-    val protagonists = lines.map(Protagonist.fromLine)
+    val protagonists = lines.map { Protagonist.fromLine }
     val n = askForUpdate()
     val updated = protagonists.map(updateAge(_, n))
     val newLines = updated.map(_.toLine)
@@ -20,8 +20,11 @@ object SideEffectsVersion {
     answer.toInt
   }
 
-  private def updateAge(p: Protagonist, n: Int): Protagonist =
-    p.copy(age = p.age + n)
+  private def updateAge(p: Protagonist, n: Int): Protagonist = {
+    val newAge = p.age + n
+    println(s"The age of ${p.firstName} ${p.lastName} is changed from ${p.age} to $newAge")
+    p.copy(age = newAge)
+  }
 
   private def write(path: Path, lines: List[String]): Unit =
     Files.writeString(path, lines.mkString("\n"))
--- src/main/scala/upforgrabs/SCL24374.scala
+++ src/main/scala/upforgrabs/SCL24374.scala
@@ -1,16 +1,16 @@
 package upforgrabs
 
-case class Foo(n: Int)
+class Foo(n: Int)
 
 object SCL24374 {
-  def add(n: Int, arg1: Foo, arg2: Int => Foo, p: Int): Int = n + p
+  def add(n: Int)(p: Int): Int = n + p
 
   @main def main(): Unit = {
     // 1. add a second parameter, e.g. add(1, 2)
     // 2. Show context actions
     // 3. A new context action should add the parameter with its inferred type to the method
     val r = (x: Int) => Foo(x)
-    val res = add(1, Foo(1), r, 2)
+    val res = add(1)(Foo(1), r, 2)
 
     println(res)
   }
